ebc69b9d90d1bb2b671cf9b3e1eb8e51f5cedd0a
KennuByte <kenoleander@gmail.com>
2024-10-12 10:07:02 +0300

    Add back-end and fily initial files and configurations

--- fily/vite.config.ts
+++ fily/vite.config.ts
@@ -1,20 +1,13 @@
-import { defineConfig } from 'vite';
-import solidPlugin from 'vite-plugin-solid';
-// import devtools from 'solid-devtools/vite';
+import { defineConfig } from "vite";
+import suidPlugin from "@suid/vite-plugin";
+import solidPlugin from "vite-plugin-solid";
 
 export default defineConfig({
-  plugins: [
-    /* 
-    Uncomment the following line to enable solid-devtools.
-    For more info see https://github.com/thetarnav/solid-devtools/tree/main/packages/extension#readme
-    */
-    // devtools(),
-    solidPlugin(),
-  ],
-  server: {
-    port: 3000,
-  },
+  plugins: [suidPlugin(), solidPlugin()],
   build: {
-    target: 'esnext',
+    target: "esnext",
   },
+  server: {
+    port: 3000
+  }
 });
