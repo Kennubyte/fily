849ef19210635c2816de526eb8f5fdecde4049b6
KennuByte <73755588+kennu290@users.noreply.github.com>
2024-10-25 03:22:42 +0300

    Refactor App.tsx to update button text for file download

--- src/App.tsx
+++ src/App.tsx
@@ -132,7 +132,7 @@ export default function App() {
                 type="submit"
                 class="h-12 w-full"
                 >
-                Connect {receivingFile() ? fileDownloadProgress() + "%" : ""}
+                Download {receivingFile() ? fileDownloadProgress() + "%" : ""}
               </Button>
             </form>
   
